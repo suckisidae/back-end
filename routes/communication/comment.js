3531135d3becd11b19a09cee184a90cc21ae77cc
kin9jon9 <cvj9883@naver.com>
2019-11-07 19:34:31 +0900

    write like.js

--- routes/communication/comment.js
+++ routes/communication/comment.js
@@ -56,20 +56,5 @@ router.put('/:comment_idx', async(req, res)=>{ //처음에는 해당 게시글id
 	}
 });
 
-<<<<<<< HEAD
-router.post('/', async (req, res) => {
-  const postMyCommentQuery = "INSERT INTO comment (item_idx, write_idx, text, date) VALUES  (?,?,?,?)";
-  const postMyCommentResult = await db.queryParam_Arr(postMyCommentQuery, [1,2,"감사합니다",1]);
-
-  //예외 처리
-  if(!postMyCommentResult){
-    res.status(200).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.BAD_REQUEST));
-  }else{
-    res.status(200).send(utils.successTrue(statusCode.OK , "성공했어요.", postMyCommentResult))
-  }
-
-});
-=======
->>>>>>> 5533aefd0cef8a62aea3a5b54bc3ac32c3ad4da1
 
 module.exports = router;
