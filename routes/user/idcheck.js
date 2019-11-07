0d7046440cafd271fe5d6ff726ecf019963aafb0
rdd9223 <rdd9223@naver.com>
2019-11-07 17:58:14 +0900

    add idcheck.js

--- routes/user/idcheck.js
+++ routes/user/idcheck.js
@@ -8,9 +8,19 @@ const authUtils = require('../../module/utils/authUtils');
 const upload = require('../../config/multer');
 const jwt = require('../../module/jwt');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '아이디 중복체크' });
+router.get('/', async(req, res) => {
+    const id = req.body.id;
+
+    const idCheckQuery = `SELECT id FROM user WHERE id = '${id}'`;
+    const idCheckResult = await db.queryParam_Parse(idCheckQuery);
+    console.log(idCheckResult);
+    console.log(id)
+
+    if(!idCheckResult[0]){
+        res.status(200).send(utils.successTrue(statusCode.OK, resMessage.ABLE_ID));
+    } else {
+        res.status(400).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
+    }
 });
 
 module.exports = router;
