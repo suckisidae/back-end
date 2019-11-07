070ea9fa67bb75df254e3f21a43bdc040b50821f
seyeong <tpdud1116@daum.net>
2019-11-07 17:47:37 +0900

    최신상품, 댓글 예외처리

--- routes/home/latelylist.js
+++ routes/home/latelylist.js
@@ -29,7 +29,7 @@ router.get('/', async(req, res)=>{
 
 	//임시
 	let temp = [];
-	temp.push(getRecentFoodResult);	
+	temp.push(getRecentFoodResult);
 	temp.push(getRecentITResult);
 	temp.push(getRecentClothesResult);
 	temp.push(getRecentBookResult);
