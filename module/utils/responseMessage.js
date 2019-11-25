4cf012b1a1c72e2befb80ff3eec45155c05acda6
seyeong <tpdud1116@daum.net>
2019-11-25 23:19:40 +0900

    add star

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -113,5 +113,6 @@ module.exports = {
     ALLOW_EXCHANGE_FAIL: "거래요청 수락 실패",
     ALLOW_EXCHANGE_SUCCESS: "거래요청 수락 성공",
 
-    ALREADY_STAR_DONE: "이미 별점을 준 거래입니다."
+    ALREADY_STAR_DONE: "이미 별점을 준 거래입니다.",
+    SUCCESS_UPDATE_STAR: "별점 주기 및 업데이트 성공"
 };
