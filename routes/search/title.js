f92f1d0a5a06e1df30952c1d376f82142ac14ef3
kin9jon9 <cvj9883@naver.com>
2019-11-29 18:27:29 +0900

    fix sort.js AND add method in search/files...

--- routes/search/title.js
+++ routes/search/title.js
@@ -9,17 +9,53 @@ const upload = require('../../config/multer');
 const jwt = require('../../module/jwt');
 const sort = require('../../module/quicksort');
 
-//
-router.get('/', async(req, res) => {
-    const keyword = req.query.keyword;
-    const getSearchQuery = `SELECT DISTINCT item_idx, title, user.nickname, thumbnail, text FROM item, user WHERE item.title LIKE '%${keyword}%' AND user.user_idx = item.writer_idx ORDER BY date DESC`;
-    var getSearchResult = await db.queryParam_Parse(getSearchQuery)
+// 최신순 조회
+router.get('/byDate', async (req, res) => {
+	const keyword = req.query.keyword;
+	const getSearchQuery = `SELECT DISTINCT item_idx, title, user.nickname, thumbnail, text FROM item, user WHERE item.title LIKE '%${keyword}%' AND user.user_idx = item.writer_idx ORDER BY date DESC`;
+	const getSearchResult = await db.queryParam_Parse(getSearchQuery)
 
-    if(getSearchResult[0] == null){
-        res.status(400).send(utils.successFalse(statusCode.NOT_FOUND, resMessage.SEARCH_TITLE_BAD_RESULT));
-    } else {
-        res.status(200).send(utils.successTrue(statusCode.OK, resMessage.SEARCH_TITLE_SUCCESS, getSearchResult))
-    }
+	let result = [];
+
+	for(i in getSearchResult) {
+		result[i] = getSearchResult[i];
+	}
+
+	if (getSearchResult[0] == null) {
+		res.status(400).send(utils.successFalse(statusCode.NOT_FOUND, resMessage.SEARCH_TITLE_BAD_RESULT));
+	} else {
+		res.status(200).send(utils.successTrue(statusCode.OK, resMessage.SEARCH_TITLE_SUCCESS, getSearchResult));
+	}
+});
+
+// 조회순 조회
+router.get('/byView', async (req, res) => {
+	const keyword = req.query.keyword;
+	const getSearchQuery = `SELECT DISTINCT item_idx, title, user.nickname, thumbnail, text FROM item, user WHERE item.title LIKE '%${keyword}%' AND user.user_idx = item.writer_idx ORDER BY date DESC`;
+	const getSearchResult = await db.queryParam_Parse(getSearchQuery)
+
+	const result = sort.byViews(getSearchResult);
+
+	if (getSearchResult[0] == null) {
+		res.status(400).send(utils.successFalse(statusCode.NOT_FOUND, resMessage.SEARCH_TITLE_BAD_RESULT));
+	} else {
+		res.status(200).send(utils.successTrue(statusCode.OK, resMessage.SEARCH_TITLE_SUCCESS, getSearchResult));
+	}
+});
+
+// 좋아요순 조회
+router.get('/byLike', async (req, res) => {
+	const keyword = req.query.keyword;
+	const getSearchQuery = `SELECT DISTINCT item_idx, title, user.nickname, thumbnail, text FROM item, user WHERE item.title LIKE '%${keyword}%' AND user.user_idx = item.writer_idx ORDER BY date DESC`;
+	const getSearchResult = await db.queryParam_Parse(getSearchQuery)
+
+	const result = sort.byLikes(getSearchResult);
+
+	if (getSearchResult[0] == null) {
+		res.status(400).send(utils.successFalse(statusCode.NOT_FOUND, resMessage.SEARCH_TITLE_BAD_RESULT));
+	} else {
+		res.status(200).send(utils.successTrue(statusCode.OK, resMessage.SEARCH_TITLE_SUCCESS, getSearchResult));
+	}
 });
 
 module.exports = router;
