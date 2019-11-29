ac99f01f4798669b7e9d7b92f1700e7e86428da7
kin9jon9 <cvj9883@naver.com>
2019-11-29 15:33:37 +0900

    191129

--- routes/user/doublecheck.js
+++ routes/user/doublecheck.js
@@ -13,11 +13,11 @@ router.get('/', async (req, res) => {
     const nickname = req.body.nickname;
 
     //존재 유무 확인
-    const nicknameCheckQuery = `SELECT EXISTS (SELECT nickname FROM user WHERE nickname = '${nickname}') as SUCCESS`;
-    const nicknameCheckResult = await db.queryParam_Parse(nicknameCheckQuery);
+    const nicknameCheckQuery = `SELECT EXISTS (SELECT * FROM user WHERE nickname = ?) as SUCCESS`;
+    const nicknameCheckResult = await db.queryParam_Parse(nicknameCheckQuery, [nickname]);
     
-    if (!nicknameCheckResult[0]["SUCCESS"]) {
-        res.status(200).send(utils.successTrue(statusCode.OK, resMessage.ABLE_NICKNAME));
+    if (nicknameCheckResult[0]["SUCCESS"] == 0) {
+        res.status(200).send(utils.successTrue(statusCode.OK, resMessage.ABLE_NICKNAME, nicknameCheckResult));
     } else {
         res.status(400).send(utils.successFalse(statusCode.BAD_REQUEST, resMessage.ALREADY_NICKNAME));
     }
